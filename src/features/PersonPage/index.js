9d18216c0ffa032ce9fa3cc5c5d8fbf4881eceb0
Angelika Mallek <angelika.mallek03@gmail.com>
2023-12-09 19:10:54 +0100

    added crew to PersonPage

--- src/features/PersonPage/index.js
+++ src/features/PersonPage/index.js
@@ -30,6 +30,7 @@ const PersonPage = () => {
 
     try {
         const casts = credits.cast;
+        const crews = credits.crew;
         return(
             <Container>
                 <PersonTileDetalis
@@ -51,6 +52,23 @@ const PersonPage = () => {
                             poster={cast.poster_path}
                             voteCount={cast.vote_count}
                             genres={cast.genre_ids.map((id) =>
+                                genres.genres.find((genre) =>
+                                    genre.id === id).name
+                            )}
+                        />
+                    ))}
+                </MoviesGrid>
+                <Title>Crew</Title>
+                <MoviesGrid>
+                    {crews.map((crew) => (
+                        <MovieTile
+                            key={crew.id}
+                            title={crew.title}
+                            date={crew.release_date}
+                            vote={crew.vote_average}
+                            poster={crew.poster_path}
+                            voteCount={crew.vote_count}
+                            genres={crew.genre_ids.map((id) =>
                                 genres.genres.find((genre) =>
                                     genre.id === id).name
                             )}
