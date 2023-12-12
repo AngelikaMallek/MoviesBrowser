21631b69e80f74b331c230ed8f5d1f15cef8ec5a
Angelika Mallek <angelika.mallek03@gmail.com>
2023-12-12 13:54:47 +0100

    created initial styled pagination

--- src/common/Pagiation/index.js
+++ src/common/Pagiation/index.js
@@ -1,34 +1,41 @@
 import { ButtonsWrapper, Button, Wrapper, Page, Number, TextWrapper, ArrowStyled } from "./styled";
+import { usePagination } from "./usePagination";
 
-const Pagination = () => (
-    <Wrapper>
-        <ButtonsWrapper>
-            <Button>
-                <ArrowStyled />
-                First
-            </Button>
-            <Button>
-                <ArrowStyled />
-                Previous
-            </Button>
-        </ButtonsWrapper>
-        <TextWrapper>
-            <Page>Page</Page>
-            <Number>1</Number>
-            <Page>of</Page>
-            <Number>500</Number>
-        </TextWrapper>
-        <ButtonsWrapper>
-            <Button>
-                Next
-                <ArrowStyled right/>
-            </Button>
-            <Button>
-                Last
-                <ArrowStyled right/>
-            </Button>
-        </ButtonsWrapper>
-    </Wrapper>
-)
+const Pagination = () => {
+
+    const { page, pagePrevious, pageNext, setFirstPage, setLastPage } = usePagination();
+
+    return (
+        <Wrapper>
+            <ButtonsWrapper>
+                <Button onClick={setFirstPage}>
+                    <ArrowStyled />
+                    First
+                </Button>
+                <Button onClick={pagePrevious}>
+                    <ArrowStyled />
+                    Previous
+                </Button>
+            </ButtonsWrapper>
+            <TextWrapper>
+                <Page>Page</Page>
+                <Number>{page}</Number>
+                <Page>of</Page>
+                <Number>500</Number>
+            </TextWrapper>
+            <ButtonsWrapper>
+                <Button onClick={pageNext}>
+                    Next
+                    <ArrowStyled right/>
+                </Button>
+                <Button onClick={setLastPage}>
+                    Last
+                    <ArrowStyled right/>
+                </Button>
+            </ButtonsWrapper>
+        </Wrapper>
+    )
+
+}
 
 export default Pagination;
---
+++ src/common/Pagiation/usePagination.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export const usePagination = () => {
+	const [page, setPage] = useState(1);
+
+    const pagePrevious = () => {
+        setPage((page) => page - 1);
+      };
+    
+      const pageNext = () => {
+        setPage((page) => page + 1);
+      };
+    
+      const setFirstPage = () => {
+        setPage((page) => (page = 1));
+      };
+    
+      const setLastPage = () => {
+        setPage((page) => (page = 500));
+      };
+
+	return { page, pagePrevious, pageNext, setFirstPage, setLastPage };
+};
